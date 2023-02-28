387422c361ad8dc1068b7fe1ca7cfed53755da06
Darío Scattolini <dario.scattolini@gmail.com>
2023-02-28 16:20:54 +0100

    add emojis test boilerplate

---
+++ src/test/java/app/core/SendMessageTest.java
@@ -0,0 +1,26 @@
+package app.core;
+
+import org.junit.jupiter.api.Test;
+
+class SendMessageTest {
+
+    @Test
+    public void itPrependsAWinterEmojiInWinter() {
+        // TODO()
+    }
+
+    @Test
+    public void itPrependsASpringEmojiInSpring() {
+        // TODO()
+    }
+
+    @Test
+    public void itPrependsASummerEmojiInSummer() {
+        // TODO()
+    }
+
+    @Test
+    public void itPrependsAnAutumnEmojiInAutumn() {
+        // TODO()
+    }
+}
