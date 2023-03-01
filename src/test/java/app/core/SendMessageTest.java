6fa63136f2e5e6137b0e0673e7cbbdc18000c9fb
Natalia <natalia.hernandez@clarity.ai>
2023-03-01 10:28:42 +0100

    chore: add mockito dependencies

--- src/test/java/app/core/SendMessageTest.java
+++ src/test/java/app/core/SendMessageTest.java
@@ -1,12 +1,15 @@
 package app.core;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class SendMessageTest {
 
     @Test
     public void itPrependsAWinterEmojiInWinter() {
-        // TODO()
+
     }
 
     @Test
